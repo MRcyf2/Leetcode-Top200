809d0d20eeab8337d13329822ecc49d85da34ec4
ccaiyf <496357312@qq.com>
2023-07-26 14:59:37 +0800

    7/26

---
+++ no.1两数之和.py
@@ -0,0 +1,31 @@
+'''
+Author: ccaiyf 496357312@qq.com
+Date: 2023-07-26 14:26:59
+LastEditors: ccaiyf 496357312@qq.com
+LastEditTime: 2023-07-26 14:41:50
+Description: QQ:496357312
+
+Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+'''
+
+
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        return []
+
+                
+if __name__ == '__main__':
+    nums=[2,7,11,15]
+    target=9
+    print(Solution().twoSum(nums,target))
+    print('Hello World')

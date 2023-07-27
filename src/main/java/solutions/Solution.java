fa0c747e251009340eb53b198d949bdd767cc65c
karywood <834479572@qq.com>
2023-07-27 12:07:38 +0800

    kary的题目

--- src/main/java/solutions/Solution.java
+++ src/main/java/solutions/Solution.java
@@ -168,4 +168,16 @@ public class Solution {
         }
         return resBuff.toString();
     }
+    //|0007|Reverse Integer|none|Medium|
+    public int reverse(int x) {
+        int res=0;
+        while(x!=0){
+            if(res<Integer.MIN_VALUE/10||res>Integer.MAX_VALUE/10){
+                return 0;
+            }
+            res=res*10+x%10;
+            x/=10;
+        }
+        return res;
+    }
 }

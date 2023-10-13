eadc8db4285c6ad67757637c1b6ebc40fb4d3629
ccaiyf <496357312@qq.com>
2023-10-13 11:32:11 +0800

    git test

--- py/521.最长特殊序列.py
+++ py/521.最长特殊序列.py
@@ -2,12 +2,29 @@
 Author: ccyfifi
 Date: 2023-08-14 17:12:07
 LastEditors: ccaiyf 
-LastEditTime: 2023-08-14 17:17:12
+LastEditTime: 2023-08-15 15:27:23
 Description: QQ:496357312欢迎交流
 242. 有效的字母异位词
 
 
 '''
+#2236. 判断根结点是否等于子结点之和
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        a=root.val
+        b=root.left.val
+        c=root.right.val
+        if a==b+c:
+            return True
+        else:
+            return False
